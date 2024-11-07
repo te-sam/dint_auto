211db94cf725d233b5c0efd5652cad79a8e50b2d
te-sam <art.samohwalov@yandex.ru>
2024-11-07 12:14:19 +0300

    allure attach

--- tests/test_screenshots.py
+++ tests/test_screenshots.py
@@ -17,9 +17,13 @@ def test_screenshots(driver, credentials):
         work.click_by_canvas(-400, 0)
     with allure.step('Сделать скриншот 2D сцены'):
         work.create_screenshot('img/current_screen_2d.png')
+        with open('img/current_screen_2d.png', 'rb') as f:
+            allure.attach(f.read(), name="Скриншот 2D сцены", attachment_type=allure.attachment_type.PNG)
         assert work.compare_img('img/current_screen_2d.png', 'img/screen_2d.png'), "Скриншоты не совпадают"
     with allure.step('Перейти на 3D сцену'):
         work.click_3d()
     with allure.step('Сделать скриншот 3D сцены'):
         work.create_screenshot('img/current_screen_3d.png')
+        with open('img/current_screen_3d.png', 'rb') as f:
+            allure.attach(f.read(), name="Скриншот 3D сцены", attachment_type=allure.attachment_type.PNG)
         assert work.compare_img('img/current_screen_3d.png', 'img/screen_3d.png'), "Скриншоты не совпадают"
